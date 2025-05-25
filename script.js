13932a9cef564e75d0703a3d247c6eb0db687788
faidilzahpar <paidilzahpar@gmail.com>
2025-05-25 15:21:46 +0700

    js input

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ function setUkuran(u) {
   renderInput();
   document.getElementById("hasilMatrix").innerText = "";
 }
+
+function renderInput() {
+  let html = "";
+
+  const generateMatrixHTML = (label, idPrefix) => {
+    let rows = "";
+    for (let i = 0; i < ukuran; i++) {
+      let cols = "";
+      for (let j = 0; j < ukuran; j++) {
+        cols += `<div class="col-${12 / ukuran}">
+          <input class="form-control" type="number" id="${idPrefix}${i}${j}" placeholder="0" />
+        </div>`;
+      }
+      rows += `<div class="row g-2 mb-2">${cols}</div>`;
+    }
+
+    return `<div class="col-md-6 mb-4">
+      <h4>Matriks ${label}</h4>${rows}
+    </div>`;
+  };
+
+  html += generateMatrixHTML("A", "a");
+  html += generateMatrixHTML("B", "b");
+
+  document.getElementById("matriksInput").innerHTML = html;
+}
