303f6db50a73b30e45be84902cfece1d850b82af
faidilzahpar <paidilzahpar@gmail.com>
2025-05-25 15:16:23 +0700

    delete js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-          } else {
-            entry.target.classList.remove("show"); // Reset animasi agar bisa dimainkan ulang
-          }
-        });
-      },
-      { threshold: 0.2 }
-    );
-  
-    document.querySelectorAll(".hidden").forEach((el) => observer.observe(el));
-  });
+
