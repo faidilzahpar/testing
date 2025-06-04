5609241cfe25febdc882bc76e3682100aa1f535a
faidilzahpar <paidilzahpar@gmail.com>
2025-06-04 19:03:09 +0700

    add setlist songs

---
+++ data/setlistsongs.js
@@ -0,0 +1,12 @@
+const setlistSongs = [
+  {
+    name: "Pajama Drive",
+    setlist: "pajama",
+    tracknumber: "M1",
+  },
+  { name: "Tenshi no Shippo", setlist: "pajama" },
+  { name: "Boku no Sakura", setlist: "pajama" },
+  // dst
+];
+
+export default setlistSongs;
