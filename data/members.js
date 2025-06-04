5666596c12e5c33e63a3930630630a599c09bd48
faidilzahpar <paidilzahpar@gmail.com>
2025-06-04 15:27:41 +0700

    add member data

--- data/members.js
+++ data/members.js
@@ -3,26 +3,216 @@ const allMembers = [
     name: "Feni Fitriyanti",
     image: "./assets/feni.jpg",
     isActive: true,
-    generation: "gen3"
+    generation: "gen3",
   },
   {
     name: "Shania Gracia",
     image: "./assets/gracia.jpg",
     isActive: true,
-    generation: "gen3"
+    generation: "gen3",
   },
   {
     name: "Gita Sekar Andarini",
     image: "./assets/gita.jpg",
     isActive: true,
-    generation: "gen6"
+    generation: "gen6",
   },
   {
     name: "Helisma Putri",
     image: "./assets/eli.jpg",
     isActive: true,
-    generation: "gen7"
-  }
+    generation: "gen7",
+  },
+  {
+    name: "Mutiara Azzahra",
+    image: "./assets/muthe.jpg",
+    isActive: true,
+    generation: "gen7",
+  },
+  {
+    name: "Febriola Sinambela",
+    image: "./assets/olla.jpg",
+    isActive: true,
+    generation: "gen7",
+  },
+  {
+    name: "Jessica Chandra",
+    image: "./assets/jessi.jpg",
+    isActive: true,
+    generation: "gen7",
+  },
+  {
+    name: "Angelina Christy",
+    image: "./assets/christy.jpg",
+    isActive: true,
+    generation: "gen7",
+  },
+  {
+    name: "Freya Jayawardana",
+    image: "./assets/freya.jpg",
+    isActive: true,
+    generation: "gen7",
+  },
+
+  // Gen 8
+  {
+    name: "Fiony Alveria",
+    image: "./assets/fiony.jpg",
+    isActive: true,
+    generation: "gen8",
+  },
+  {
+    name: "Cornelia Vanisa",
+    image: "./assets/oniel.jpg",
+    isActive: true,
+    generation: "gen8",
+  },
+  {
+    name: "Lulu Salsabila",
+    image: "./assets/lulu.jpg",
+    isActive: true,
+    generation: "gen8",
+  },
+
+  // Gen 9
+  {
+    name: "Indah Cahya",
+    image: "./assets/indah.jpg",
+    isActive: true,
+    generation: "gen9",
+  },
+  {
+    name: "Marsha Lenathea",
+    image: "./assets/marsha.jpg",
+    isActive: true,
+    generation: "gen9",
+  },
+  {
+    name: "Kathrina Irene",
+    image: "./assets/kathrina.jpg",
+    isActive: true,
+    generation: "gen9",
+  },
+
+  // Gen 10
+  {
+    name: "Jesslyn Elly",
+    image: "./assets/lyn.jpg",
+    isActive: true,
+    generation: "gen10",
+  },
+  {
+    name: "Aurellia",
+    image: "./assets/lia.jpg",
+    isActive: true,
+    generation: "gen10",
+  },
+  {
+    name: "Indira Seruni",
+    image: "./assets/indira.jpg",
+    isActive: true,
+    generation: "gen10",
+  },
+  {
+    name: "Amanda Sukma",
+    image: "./assets/amanda.jpg",
+    isActive: true,
+    generation: "gen10",
+  },
+  {
+    name: "Gabriela Abigail",
+    image: "./assets/ella.jpg",
+    isActive: true,
+    generation: "gen10",
+  },
+  {
+    name: "Raisha Syifa",
+    image: "./assets/raisha.jpg",
+    isActive: true,
+    generation: "gen10",
+  },
+
+  // Gen 11
+  {
+    name: "Cynthia Yaputera",
+    image: "./assets/cynthia.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Anindya Ramadhani",
+    image: "./assets/anin.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Dena Natalia",
+    image: "./assets/danella.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Desy Natalia",
+    image: "./assets/daisy.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Greesella Adhalia",
+    image: "./assets/greesel.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Alya Amanda",
+    image: "./assets/alya.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Celline Thefani",
+    image: "./assets/elin.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Grace Octaviani",
+    image: "./assets/gracie.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Michelle Alexandra",
+    image: "./assets/michie.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Cathleen Nixie",
+    image: "./assets/cathy.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Chelsea Davina",
+    image: "./assets/chelsea.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+  {
+    name: "Gendis Mayrannisa",
+    image: "./assets/gendis.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
+
+  // Gen 12
+  {
+    name: "Aurhel Alana",
+    image: "./assets/lana.jpg",
+    isActive: true,
+    generation: "gen11",
+  },
 ];
 
 export default allMembers;
