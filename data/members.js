99a87132a392782bbadcc4566af2f6d5ecf1d6b0
faidilzahpar <paidilzahpar@gmail.com>
2025-06-04 11:50:37 +0700

    add member data

---
+++ data/members.js
@@ -0,0 +1,28 @@
+const allMembers = [
+  {
+    name: "Feni Fitriyanti",
+    image: "./assets/feni.jpg",
+    isActive: true,
+    generation: "gen3"
+  },
+  {
+    name: "Shania Gracia",
+    image: "./assets/gracia.jpg",
+    isActive: true,
+    generation: "gen3"
+  },
+  {
+    name: "Gita Sekar Andarini",
+    image: "./assets/gita.jpg",
+    isActive: true,
+    generation: "gen6"
+  },
+  {
+    name: "Helisma Putri",
+    image: "./assets/eli.jpg",
+    isActive: true,
+    generation: "gen7"
+  }
+];
+
+export default allMembers;
